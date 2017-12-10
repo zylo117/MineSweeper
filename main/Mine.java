4543fb9d8bc0f759b32661e1b29ff9d271214a52
zylo117 <zylo117@hotmail.com>
2017-12-10 15:08:27 +0800

    Init

---
+++ main/Mine.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Mine {
+	public static int[] setMine(Object[][] statusMap) {
+		
+		
+		return null;
+	}
+	
+	public static void judgeSingleSquare(int rowNO, int colNO) {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+}
---
+++ tool/RemoveDuplicateOffList.java
@@ -0,0 +1,20 @@
+package tool;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class RemoveDuplicateOffList {
+	public static List<Point> remove(List list) {
+		for (int i = 0; i < list.size() - 1; i++) {
+			for (int j = i + 1; j < list.size(); j++) {
+				if (list.get(i).equals(list.get(j))) {
+					list.remove(i);
+				}
+			}
+		}
+		return list;
+	}
+}
